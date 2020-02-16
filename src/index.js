3a5cea18ad2ea2f84452dba2513adb837b6bb5ef
Michael Reich <m.reich@digitalwunder.io>
2020-02-16 18:56:43 +0100

    added react ga

--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import Routes from './routes';
 
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-158657525-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 ReactDOM.render(<Routes />, document.getElementById('root'));
