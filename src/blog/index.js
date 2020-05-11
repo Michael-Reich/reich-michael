e03b793f3debcae9f79b858a25a1614dd74796f3
Michael Reich <m.reich@digitalwunder.io>
2020-05-11 22:49:40 +0200

    challange added

--- src/blog/index.js
+++ src/blog/index.js
@@ -1,8 +1,16 @@
 export const BlogItems = [
     {
-        title: 'Hallo Welt',
-        slug: 'hello-welt',
-        previewText: 'Neustart meiner persönliche Seite.',
-        markdown: 'blog/hallo-welt.md'
+      title: 'Hallo Welt',
+      slug: 'hello-welt',
+      date: new Date('08.01.2020'),
+      previewText: 'Neustart meiner persönliche Seite.',
+      markdown: 'blog/hallo-welt.md'
+    },
+    {
+      title: '30 Tage Challange',
+      slug: '30-tage-challange',
+      date: new Date('11.05.2020'),
+      previewText: 'Mein neuer Log für meine Ziele.',
+      markdown: 'blog/2020-05/30-Tage-Challange.md'
     }
 ]
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,7 +13,7 @@ function Home() {
                     <h1>Servus!</h1>
                     <p>Mein Name ist Michael Reich, ich bin Gründer, Fitness-Junkie und mache gern Musik.</p>
                     <p>Ein Großteil meiner Zeit widme ich dem Plan einer Weltherrschaft in Form meiner Firma <a href="https://wunderlabs.io" target="_blank" rel="noopener noreferrer">wunderlabs.io</a>.</p>
-                    <p>Sontige Kreative ergüsse findet ihr in meinem Blog oder auf meine Soundcloud Seite <a href="https://soundcloud.com/bonzmann" target="_blank" rel="noopener noreferrer">Bonzmann</a></p>
+                    <p>Sontige Kreative ergüsse findet ihr in meinem Blog oder auf meine Soundcloud Seite <a href="https://soundcloud.com/hairymike" target="_blank" rel="noopener noreferrer">Hairy Mike</a></p>
                     <Link className="btn btn-primary" to="/profile">Mehr über mich</Link>
                 </Col>
                 <Col md={6}>
