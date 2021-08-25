ae2f2e29f485d3e88f3dbf491f4ddc2752bafbd2
Michael Reich <m.reich@digitalwunder.io>
2021-08-25 10:17:17 +0200

    fixed some text

--- src/blog/index.js
+++ src/blog/index.js
@@ -5,33 +5,5 @@ export const BlogItems = [
       date: new Date('08-01-2020'),
       previewText: 'Neustart meiner persönliche Seite.',
       markdown: 'blog/hallo-welt.md'
-    },
-    {
-      title: '30 Tage Challange',
-      slug: '30-tage-challange',
-      date: new Date('05-11-2020'),
-      previewText: 'Mein neuer Log für meine Ziele.',
-      markdown: 'blog/2020-05/30-Tage-Challange.md'
-    },
-    {
-      title: 'Tag 1/30',
-      slug: 'tag-1',
-      date: new Date('05-12-2020'),
-      previewText: 'Erster Tag, erste Erkenntnisse.',
-      markdown: 'blog/2020-05/Tag-1-von-30.md'
-    },
-    {
-      title: 'Tag 2/30',
-      slug: 'tag-2',
-      date: new Date('05-13-2020'),
-      previewText: 'Zweiter Tag: Voller Energie!',
-      markdown: 'blog/2020-05/Tag-2-von-30.md'
-    },
-    {
-      title: 'Tag 3 & 4/30',
-      slug: 'tag-3-4',
-      date: new Date('05-15-2020'),
-      previewText: 'Nichts läuft wie geplant.',
-      markdown: 'blog/2020-05/Tag-3-4-von-30.md'
     }
 ]
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,9 +11,7 @@ function Home() {
             <Row className="align-items-center">
                 <Col md={6}>
                     <h1>Servus!</h1>
-                    <p>Mein Name ist Michael Reich, ich bin Gründer, Fitness-Junkie und mache gern Musik.</p>
-                    <p>Ein Großteil meiner Zeit widme ich dem Plan einer Weltherrschaft in Form meiner Firma <a href="https://wunderlabs.io" target="_blank" rel="noopener noreferrer">wunderlabs.io</a>.</p>
-                    <p>Sontige Kreative ergüsse findet ihr in meinem Blog oder auf meine Soundcloud Seite <a href="https://soundcloud.com/hairymike" target="_blank" rel="noopener noreferrer">Hairy Mike</a></p>
+                    <p>Text folgt ...</p>
                     <Link className="btn btn-primary" to="/profile">Mehr über mich</Link>
                 </Col>
                 <Col md={6}>
